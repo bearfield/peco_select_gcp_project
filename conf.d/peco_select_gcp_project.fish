b39556eb235ebbc42fe3c27a3f019f33ffb0a336
kumanoryo <r-kumano@groovenauts.jp>
2018-10-24 20:35:25 +0900

    Add remove function.

--- conf.d/peco_select_gcp_project.fish
+++ conf.d/peco_select_gcp_project.fish
@@ -1,2 +1,11 @@
 # Set up an easy-to-use alias.
-alias gpj "peco_select_gcp_project"
+set plugin_alias "gpj"
+
+# enable alias.
+alias $plugin_alias "peco_select_gcp_project"
+
+# disable alias, when uninstall plugin.
+set -l name (basename (status -f) .fish){_uninstall}
+function $name
+  alias -e $plugin_alias
+end
--- conf.d/peco_select_gcp_project_key_bindings.fish
+++ conf.d/peco_select_gcp_project_key_bindings.fish
@@ -1,2 +1,11 @@
 # Set up an easy-to-use keybind.
-bind \cgp peco_select_gcp_project
+set keybind \cgp
+
+# enable keybind.
+bind $keybind peco_select_gcp_project
+
+## disable keybind, when uninstall plugin.
+set -l name (basename (status -f) .fish){_uninstall}
+function $name
+  bind -e $keybind
+end
